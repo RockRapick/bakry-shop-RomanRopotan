c687393f26e82a5f331feef93c0ed63bb9b89a27
RomanRopotan <zmei2000.25@gmail.com>
2025-06-19 10:43:59 +0300

    v1

---
+++ src/utils/paths.ts
@@ -0,0 +1,11 @@
+export enum Paths {
+    HOME = '/',
+    ORDERS = 'orders',
+    PRODUCTS = 'products',
+    CART = 'cart',
+    CUSTOMERS = 'customers',
+    BREAD = 'bread',
+    DAIRY = 'dairy',
+    ERROR = 'error',
+    BACK = 'back'
+}
